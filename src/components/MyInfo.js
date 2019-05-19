75c4f32bbb586e79e962615ebc0bd3adb9561aad
NabidAlam <msa.nabid007@gmail.com>
2019-05-19 22:11:44 +0600

    added mother child component

---
+++ src/components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function MyInfo(){
+    return (
+        <div>
+            <h1>Hi Nabid Alam</h1>
+            <p>I am working as a Software Engineer</p>
+            <ul>
+                <li>Html</li>
+                <li>CSS</li>
+                <li>JS</li>
+
+            </ul>
+        </div>
+    )
+}
+
+
+export default MyInfo
