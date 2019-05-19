d55d46114c461d8de243e507b0b6e528410e2614
NabidAlam <msa.nabid007@gmail.com>
2019-05-19 23:15:23 +0600

    checkbox

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Header(){
+    return (
+
+        <nav>
+            <h1>Hello</h1>
+
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JQuery</li>
+                <li>JavaScript</li>
+            </ul>
+        </nav>
+    )
+}
+
+
+
+export default Header
+
---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MainContent(){
+    return (
+        
+    <main>
+      <p>Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic and numeric characters, between two or more users of mobile devices, desktops/laptops, or other type of compatible computer.</p>
+    </main>
+
+
+    )
+}
+
+export default MainContent
